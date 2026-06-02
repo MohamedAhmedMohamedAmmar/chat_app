41a0532b9990103df007ba394701038d5f26c4e4
Mohamed <104022503+MohamedAhmedMohamedAmmar@users.noreply.github.com>
2026-06-02 22:50:59 +0300

    Remove production check for server listening

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -106,9 +106,7 @@ app.use(
   }
 );
 
-if (process.env.NODE_ENV !== "production") {
   httpServer.listen(process.env.PORT || 5001, () => {
     console.log(`Server running on port ${process.env.PORT || 5001}`);
   });
-}
 export default app;
