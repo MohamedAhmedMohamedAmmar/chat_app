63aeb3bdd42bef6965b271d7a8fa960f1e8828e1
Mohamed <104022503+MohamedAhmedMohamedAmmar@users.noreply.github.com>
2026-06-02 22:56:04 +0300

    update port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -106,7 +106,7 @@ app.use(
   }
 );
 
-  httpServer.listen(process.env.PORT || 5001, () => {
-    console.log(`Server running on port ${process.env.PORT || 5001}`);
+  httpServer.listen(process.env.PORT || 8080, () => {
+    console.log(`Server running on port ${process.env.PORT || 8080}`);
   });
 export default app;
